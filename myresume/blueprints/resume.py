1ef1d29d29d0b1002b41edd464465a562d82622e
jondkelley <jonk@omg.lol>
2021-03-15 21:16:39 -0500

    Add template handling to markdown generator; update resume template

--- myresume/blueprints/resume.py
+++ myresume/blueprints/resume.py
@@ -16,13 +16,6 @@ myresume = Blueprint('myresume', __name__)
 with open('/resume.json', 'r') as outfile:
     resume = json_load(outfile)
 
-def generate_markdown():
-    jinja2.filters.FILTERS['resume_date'] = filter_resume_date
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader("/"))
-    template = env.get_template("resume.md.jinja2")
-    markdown = template.render(resume=resume)
-    return markdown
-
 @myresume.route("/", methods=['GET', 'POST'])
 def login():
     """
@@ -37,23 +30,43 @@ def term():
     """
     return render_template('term.html')
 
+def generate_markdown():
+    """
+    generate resume in markdown format
+    """
+    jinja2.filters.FILTERS['resume_date'] = filter_resume_date
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader("/"))
+    template = env.get_template("resume.md.jinja2")
+    markdown = template.render(resume=resume)
+    return markdown
+
 @myresume.route("/resume.md")
-def serve_markdown():
+def resume_markdown():
     """
     generates a rendered markdown document from resume
     """
-    markdown = generate_markdown()
-    response = make_response(markdown, 200)
-    response.mimetype = "text/plain"
-    return response
+    theme = request.args.get('theme', 'markdown3')
+    render = request.args.get('render', False)
+    if not render:
+        response = make_response(generate_markdown(), 200)
+        response.mimetype = "text/plain"
+        return response
+    else:
+        html_body = markdown.markdown(generate_markdown(), extensions=['fenced_code', 'codehilite'])
+        stylesheet = f'<link rel="stylesheet" href="/static/css/markdown/{theme}.css"/>'
+        return f'{stylesheet}\n\n{html_body}'
 
-@myresume.route("/resume.md/render")
-def serve_markdown_rendered():
+@myresume.route("/resume.pdf")
+def resume_pdf():
     """
-    generate rendered markdown
+    generates a rendered PDF file from a markdown base
     """
-    html = markdown.markdown(generate_markdown(), extensions=['fenced_code', 'codehilite'])
-    return html
+    theme = request.args.get('theme', 'markdown3')
+    render = request.args.get('render', False)
+    html_body = markdown.markdown(generate_markdown(), extensions=['fenced_code', 'codehilite'])
+    stylesheet = f'<link rel="stylesheet" href="/static/css/markdown/{theme}.css"/>'
+    return f'{stylesheet}\n\n{html_body}'
+
 
 @myresume.route("/resume.json", methods=['GET', 'POST'])
 def resume_json():
