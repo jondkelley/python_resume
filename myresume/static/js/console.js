cb7981130c76d7d4ca25f134f5c0ff6db3b24d9a
toor <jonk@omg.lol>
2021-03-16 20:43:33 -0500

    Add console message

---
+++ myresume/static/js/console.js
@@ -0,0 +1,16 @@
+const styles0 = [
+  'color: black',
+  'background: gray',
+  'font-size: 10px',
+  'padding: 10px',
+].join(';');
+
+const styles1 = [
+  'color: black',
+  'background: gray',
+  'font-size: 15px',
+  'padding: 10px',
+].join(';');
+
+console.log('%c If you\'re this interested in my code...', styles0);
+console.log('%c 😉 You should hire me! 😉', styles1);
