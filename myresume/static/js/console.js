d3c7bab723076e1d27ca84591fbea5673e09c789
toor <jonk@omg.lol>
2021-03-16 21:17:11 -0500

    Add blur effect to profile image

--- myresume/static/js/console.js
+++ myresume/static/js/console.js
@@ -14,3 +14,14 @@ const styles1 = [
 
 console.log('%c If you\'re this interested in my code...', styles0);
 console.log('%c 😉 You should hire me! 😉', styles1);
+
+
+function yourFunction() {
+      const hide = document.getElementById("hide");
+      hide.className =
+        hide.className === "hide"
+          ? (hide.className = "example removeHide")
+          : (hide.className = "hide");
+      setTimeout(yourFunction, 1500);
+    }
+    yourFunction();
