5880ef466b268b5193f5d21f349599acd5bd7e93
jondkelley <jonk@omg.lol>
2021-04-12 22:54:51 -0500

    Cleanup sidecar entrypoint script

--- pandoc-sidecar/entrypoint.sh
+++ pandoc-sidecar/entrypoint.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
-# wait until flask pod starts
+# wait for flask pod to start
 sleep 5
 
 while(true); do
-  echo "----"
-  echo Waking up -- $(date -u)
-  echo "----"
+  echo "$(date -u) -- Waking up"
   tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
   pushd $tmp_dir
       curl http://$MYRESUME_HOST/resume.md/ > resume.md
       styleformats="html pdf docx"
       for format in $styleformats; do
-        pandoc -s resume.md -c /root/markdown8.css -o /pandoc/resume.$format
+        pandoc -s resume.md -c /root/markdown8.css -o /pandoc/resume.$format --metadata title="resume"
       done
 
-      formats="tex epub odt rst"
-      for format in $formats; do
-        pandoc -s resume.md -o /pandoc/resume.$format
-      done
-
-      txtformats="man jira ipynb commonmark biblatex bibtex native"
+      txtformats="tex epub odt rst man jira ipynb commonmark biblatex bibtex native"
       for format in $txtformats; do
-        pandoc -s resume.md -o /pandoc/resume.$format.txt
+        pandoc -s resume.md -o /pandoc/resume.$format.txt --metadata title="resume"
       done
       rm resume.md
   popd
   rm -rf $tmp_dir
-  echo "Waiting 10 seconds to regenerate resume(s)"
+  echo "$(date -u) -- Sleeping 10 seconds to regenerate resume(s)"
   sleep 10
 done
