672418083457d6556360f6863ce5c0f55f5fe124
Jonathan D Kelley <jonathan.kelley@logdna.com>
2021-04-13 00:14:55 -0500

    Update entrypoint.sh

--- pandoc-sidecar/entrypoint.sh
+++ pandoc-sidecar/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# file: entrypoint.sh
+# description: periodically regenerate the static resume for the shared volume with the resume container
 
 # wait for flask pod to start
 sleep 5
@@ -11,20 +13,22 @@ while(true); do
   pushd $tmp_dir
       curl http://$MYRESUME_HOST/resume.md/ > resume.md
 
-      echo -e "\n<br>Generation counter:" $(( count++ )) >> resume.md
+      echo -e "\n\n<br>Generation:" $(( count++ )) >> resume.md
+
+      echo -e "\n\n<br>ContainerId: $(hostname) Load: $(cat /proc/loadavg | awk '{ print $1 , $2 , $3 }')" >> resume.md
 
       styleformats="html pdf docx"
       for format in $styleformats; do
-        pandoc -s resume.md -c /root/markdown8.css -o /pandoc/resume.$format --metadata title="resume"
+        pandoc -s resume.md -c /root/markdown8.css -o /pandoc/resume.$format --metadata title=" "
       done
 
       txtformats="tex epub odt rst man jira ipynb commonmark biblatex bibtex native"
       for format in $txtformats; do
-        pandoc -s resume.md -o /pandoc/resume.$format.txt --metadata title="resume"
+        pandoc -s resume.md -o /pandoc/resume.$format.txt --metadata title=" "
       done
       rm resume.md
   popd
   rm -rf $tmp_dir
-  echo "$(date -u) -- Sleeping 10 seconds to regenerate resume(s)"
+  echo "$(date -u) -- Sleep 10 seconds..."
   sleep 10
 done
