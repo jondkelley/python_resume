766121d1dc5ebc60d8e3e8423bf476c352877f1f
Jonathan D Kelley <jonathan.kelley@logdna.com>
2021-04-13 15:18:57 -0500

    Update entrypoint.sh

--- pandoc-sidecar/entrypoint.sh
+++ pandoc-sidecar/entrypoint.sh
@@ -5,13 +5,14 @@
 # wait for flask pod to start
 sleep 5
 
-count=1;
+count=1
+sleeptime=30
 
 while(true); do
-  echo "$(date -u) -- Waking up"
+  echo "$(date -u) -- Waking up to generate resumes"
   tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
   pushd $tmp_dir
-      curl http://$MYRESUME_HOST/resume.md/ > resume.md
+      curl -s http://$MYRESUME_HOST/resume.md/ > resume.md
 
       echo -e "\n\n<br>Generation:" $(( count++ )) >> resume.md
 
@@ -19,11 +20,12 @@ while(true); do
 
       formats="html pdf docx tex epub odt rst man jira ipynb commonmark biblatex bibtex native"
       for format in $formats; do
+        echo "EXEC: pandoc -s resume.md -c /root/markdown8.css -o /pandoc/resume.$format"
         pandoc -s resume.md -c /root/markdown8.css -o /pandoc/resume.$format --metadata title=" "
       done
       rm resume.md
   popd
   rm -rf $tmp_dir
-  echo "$(date -u) -- Sleep 10 seconds..."
-  sleep 10
+  echo "$(date -u) -- Sleep $sleeptime seconds..."
+  sleep $sleeptime
 done
